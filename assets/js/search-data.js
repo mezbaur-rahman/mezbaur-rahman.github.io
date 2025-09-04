baaed0eccc64b2611a6cec1b05f2558a0a6f27de
mezbaur-rahman <mezbaur00797@gmail.com>
2025-09-04 17:58:04 +0000

    Deploying to gh-pages from @ mezbaur-rahman/mezbaur-rahman.github.io@60ac13c682dcbc4dc9091383d452bb6d17e7aec2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "More to Add.",
+          description: "More to Add",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
